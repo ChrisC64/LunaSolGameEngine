5f698f481d649a3c176ab73cfe44df54b52c80ff
Christopher Carlos <christopher.carlos@outlook.com>
2023-12-24 00:19:33 -0800

    - Added additional error handling parsers for HRESULT codes for DXGI, D3D12, and D3D11 - Added Input to the ErrorCategory

--- LunaSolGameEngine/inc/platform/Windows/Win32/WinApiUtils.h
+++ LunaSolGameEngine/inc/platform/Windows/Win32/WinApiUtils.h
@@ -9,22 +9,23 @@
 #include <Windows.h> 
 namespace LS::Win32
 {
-	[[nodiscard]] inline std::string HrToString(HRESULT hr)
-	{
-		return fmt::format("HRESULT of {:#x}", static_cast<UINT>(hr));
-	}
+    [[nodiscard]] inline std::string HrToString(HRESULT hr)
+    {
+        return fmt::format("HRESULT of {:#x}", static_cast<UINT>(hr));
+    }
 
-	[[nodiscard]] inline std::wstring HrToWString(HRESULT hr)
-	{
-		return fmt::format(L"HRESULT of {:#x}", static_cast<UINT>(hr));
-	}
+    [[nodiscard]] inline std::wstring HrToWString(HRESULT hr)
+    {
+        return fmt::format(L"HRESULT of {:#x}", static_cast<UINT>(hr));
+    }
 
-	[[nodiscard]] inline HANDLE CreateEventHandler(LPCWSTR name = nullptr, BOOL isManualReset = FALSE, BOOL isSignaled = FALSE) noexcept
-	{
-		HANDLE eventHandle;
-		eventHandle = ::CreateEventW(NULL, isManualReset, isSignaled, name);
-		assert(eventHandle && "Failed to create fence event.");
+    [[nodiscard]] inline HANDLE CreateEventHandler(LPCWSTR name = nullptr, BOOL isManualReset = FALSE, BOOL isSignaled = FALSE) noexcept
+    {
+        HANDLE eventHandle;
+        eventHandle = ::CreateEventW(NULL, isManualReset, isSignaled, name);
+        assert(eventHandle && "Failed to create fence event.");
 
-		return eventHandle;
-	}
+        return eventHandle;
+    }
+
 }
